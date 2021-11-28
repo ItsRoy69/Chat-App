db89110ccfacd3ac56761f13e400d27e47b77390
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-11-28 23:19:44 +0530

    Update ChatFeed.jsx

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,22 +3,61 @@ import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
 
 const ChatFeed = (props) => {
-     const { chats, aciveChat, userName, messages } = props;
+    const {
+      chats,
+      activeChat,
+      userName,
+      messages
+    } = props;
 
-     const chat = chats && chats[activeChat];
+    const chat = chats && chats[activeChat];
 
     const renderMessages = () => {
       const keys = Object.keys(messages);
-    }
 
-      return (
-        <div>
-              ChatFeed
-        </div>
+      return keys.map((key, index) => {
+          const message = messages[key];
+          const lastMessageKey = index === 0 ? null : keys[index - 1]
+          const isMyMessage = userName === message.sender.username;
 
 
+          return (
+            <div key = {`msg_${index}`}   style = {{ width: '100%' }} >
+                <div className = "message-block" >
+                 {
+                     isMyMessage
+                     ? <MyMessage />
+                     : <TheirMessage />
+                 }
+                </div>
+            <div className = "read-receipts" style = {{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }} >
+              read-receipts
+            </div>
+          </div>
+        );
+     })
+  }
+
+
+  if (!chat) return 'Loading...';
+
+  return (
+    <div className="chat-feed">
+     <div className="chat-title-container">
+       <div className="chat-title">{chat?.title}</div>
+         <div className="chat-subtitle">
+                 {chat.people.map((person) => ` ${person.person.username}`)}
+         </div>
+     </div>
+     {renderMessages()}
+
+     <div style={{ height: '100px' }} />
+         <div className="message-form-container">
+            <MessageForm {...props} chatId={activeChat} />
+         </div>
+     </div>
       );
 
-}
+    }
 
-export default ChatFeed;
+    export default ChatFeed;
