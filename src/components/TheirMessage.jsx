718c7ff526740944a3a32718ab0aa5df9383a10f
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-11-29 23:04:31 +0530

    update

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,11 +1,30 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+      const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
 
       return (
-        <div>
-              TheirMessage
-        </div>
-      );
+        <div className="message-row">
+        {isFirstMessageByUser && (
+           <div
+               className="message-avatar"
+               style={{ backgroundImage: `url(${message?.sender?.avatar})`  }}
+            />
+        )}
 
-}
+        {message?.attachments?.length > 0
+          ? (
+              <img
+                  src={message.attachments[0].file}
+                  alt = "message-attachment"
+                  className = "message-image"
+                  style = {{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+              />
+            ) : (
+              <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+                   {message.text}
+              </div>
+
+            )
+        }
+        </div>
 
 export default TheirMessage;
